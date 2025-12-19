0d76d121b5e5ff904a02a22029b901350b27e524
Ilangovan S <ilangovan9790@gmail.com>
2025-12-19 22:07:07 +0530

    The Test Analytics for Admin only has been integrated

--- api/routes.py
+++ api/routes.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from api.health import router as health_router
 from api.auth_routes import router as auth_router
 from api.decision_routes import router as decision_router
+from api.analytics_routes import router as analytics_router
 
 router = APIRouter()
 
 router.include_router(health_router)
 router.include_router(auth_router)
 router.include_router(decision_router)
+router.include_router(analytics_router)
